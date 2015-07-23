3fc3f45bae865717cf79e76d587805ecb5518c41
Moritz Nisblé <moritz.nisble@gmx.de>
2015-07-23 18:15:18 +0200

    Removed Qt containers and pointers; Added algorithm to match built-in Courses from coursefiles with that already in the Db

--- src/datamodel.cpp
+++ src/datamodel.cpp
@@ -7,6 +7,8 @@
 
 #include "datamodel.hpp"
 
+#include <map>
+
 #include <QDir>
 #include <QDebug>
 
@@ -18,19 +20,20 @@ namespace qtouch
 namespace
 {
 
-ConstCourseMap createCourseMap(const ConstCourseList& list)
+std::map<QUuid, std::shared_ptr<Course>> make_map(const std::vector<std::shared_ptr<Course>>& list)
 {
-	ConstCourseMap map;
-	foreach(const ConstCoursePtr & c, list)
+	std::map<QUuid, std::shared_ptr<Course>> map;
+	for (const auto& c : list)
 	{
-		map.insert(c->getId(), c);
+		map.insert(std::make_pair(c->getId(), c));
 	}
 	return map;
 }
 
 } /* namespace anonymous */
 
-DataModel::DataModel()
+DataModel::DataModel(QObject* parent) :
+	QObject(parent)
 {
 }
 
@@ -43,93 +46,165 @@ void DataModel::init() throw (Exception)
 	// Get all course files from the resources
 	QDir coursepath(QStringLiteral(":/courses"), "*.xml", QDir::Name | QDir::IgnoreCase, QDir::Files);
 	QStringList sourceFileList;
-	foreach(QString const & s, coursepath.entryList())
-	{
+	for (const auto& s : coursepath.entryList())
 		sourceFileList.append(coursepath.filePath(s));
-	}
 
-	// FIXME: Might throw
-	xml::ValidatorPtr validator;
-
-	try
-	{
-		validator = xml::createValidator(QStringLiteral(":/courses/course.xsd"));
-	}
-	catch (Exception& e)
-	{
-		qCritical() << e.message();
-		throw;
-	}
+	// Parse courses
+	std::vector<std::shared_ptr<Course>> parsedCourses;
 
 	xml::ParseResult result;
 	QString message;
 
-	// Parse all courses and add it to a list
+	// Create a validator
+	auto validator = xml::createValidator(QStringLiteral(":/courses/course.xsd"));
+
 	QStringListIterator it(sourceFileList);
 	while (it.hasNext())
 	{
-		CoursePtr course;
-		try
+		auto course = xml::parseCourse(it.next(), *validator, &result, &message);
+		if (result != xml::Ok)
 		{
-			course = xml::parseCourse(it.next(), validator, &result, &message);
-			if (result != xml::Ok)
-			{
-				qWarning() << "Result:" << result << " " << message.toLatin1().data();
-			}
-
-			//			beginInsertRows(QModelIndex(), mCourses.size(), mCourses.size());
-
-			mCourses.append(course);
-
-			//			endInsertRows();
+			qWarning() << "XML parser result:" << result << " " << message.toLatin1().data();
 		}
-		catch (Exception& e)
-		{
-			qCritical() << e.message();
-			throw;
-		}
+		parsedCourses.push_back(course);
 	}
 
-	std::sort(mCourses.begin(), mCourses.end(), CourseListAscTitle());
+	// Sorting
+	std::sort(parsedCourses.begin(), parsedCourses.end(), CourseListAscTitle());
 
-	mCourseMap = createCourseMap(mCourses);
-}
+	// Calculate a hash
+	auto parsedHash = hash(parsedCourses.begin(), parsedCourses.end());
 
-int DataModel::getCourseCount() const
-{
-	return mCourses.size();
+	// Initialize the database
+	/* XXX: Use QStandardPaths::DataLocation when < 5.4
+	 * else QStandardPaths::AppDataLocation */
+	db.reset(new Db(QString("QTouch.sqlite")));
+
+	// Read the hash of the build-in courses from the database
+	QByteArray dbHash;
+	// XXX: DEBUGGING: Force recreation
+//	db->open(QString(), true);
+	db->open(QString());
+
+	dbHash = db->getCourseHash();
+
+	// Compare (On match, loading from Db is redundant)
+	if (parsedHash != dbHash)
+	{
+		qWarning() << "Built-in courses in database differ from courses files: Update needed.";
+
+		std::vector<std::shared_ptr<Course>> dbCourses;
+
+		// Read all built-in courses from the database
+		dbCourses = db->courses(Db::BuiltIn);
+
+		// Get the lessons
+		for (auto& pC : dbCourses)
+			db->replaceLessons(*pC);
+
+		// Sorting
+		std::sort(dbCourses.begin(), dbCourses.end(), CourseListAscTitle());
+
+		// Match the Courses from the XML files against the courses in the database
+
+		// Make a working copy
+		auto dbCp = dbCourses;
+
+		// Iterate over the source courses
+		for (auto pIter = parsedCourses.begin(); pIter != parsedCourses.end(); ++pIter)
+		{
+			bool action = false;
+
+			for (auto dIter = dbCp.begin(); dIter != dbCp.end(); ++dIter)
+			{
+				// Compare IDs
+				if ((*pIter)->getId() == (*dIter)->getId())
+				{
+					// if ID are equal -> compare values
+					if ((**pIter) != (**dIter))
+					{
+						// if unequal -> update of destination needed
+						qDebug() << "Updating Course:" << (*pIter)->getId().toString() << " (" << (*pIter)->getTitle() << ")";
+						db->update(**pIter);
+					}
+
+					// remove from working copy
+					dbCp.erase(dIter);
+					action = true;
+					break;
+				}
+			}
+
+			// if course not found in database -> insert
+			if (!action)
+			{
+				qDebug() << "Inserting new Course:" << (*pIter)->getId().toString() << " (" << (*pIter)->getTitle() << ")";
+				db->insert(**pIter);
+			}
+		}
+
+		// the entries left in the destination copy must be redundant -> remove
+		for (const auto& d : dbCp)
+		{
+			qDebug() << "Deleting old Course:" << d->getId().toString() << " (" << d->getTitle() << ")";
+			db->deleteCourse(d->getId());
+		}
+
+		// Read all built-in courses from the database
+		auto written = db->courses(Db::BuiltIn);
+
+		// Get the lessons
+		for (auto& pC : written)
+			db->replaceLessons(*pC);
+
+		std::sort(written.begin(), written.end(), CourseListAscTitle());
+
+		auto writtenHash = hash(written.begin(), written.end());
+		if (writtenHash != parsedHash)
+		{
+			throw Exception("Hash mismatch after database update");
+		}
+
+		// If everything went fine, update the hash in the database
+		db->setCourseHash(writtenHash);
+	}
+
+	mCourses = parsedCourses;
+
+	// Create the course map
+	mCourseMap = make_map(mCourses);
 }
 
 QString DataModel::getCourseTitle(const QUuid& courseId) const
 {
-	if(isValidCourse(courseId))
-		return mCourseMap.value(courseId)->getTitle();
+	if (isValidCourse(courseId))
+		return mCourseMap.at(courseId)->getTitle();
 	else
 		return QString();
 }
 
 QString DataModel::getCourseDescription(const QUuid& courseId) const
 {
-	if(isValidCourse(courseId))
-		return mCourseMap.value(courseId)->getDescription();
+	if (isValidCourse(courseId))
+		return mCourseMap.at(courseId)->getDescription();
 	else
 		return QString();
 }
 
 bool DataModel::isCourseBuiltin(const QUuid& courseId) const
 {
-	if(isValidCourse(courseId))
-		return mCourseMap.value(courseId)->isBuiltin();
+	if (isValidCourse(courseId))
+		return mCourseMap.at(courseId)->isBuiltin();
 	else
 		return false;
 }
 
-CoursePtr DataModel::getCourseCopy(const QUuid& courseId) const
+std::shared_ptr<Course> DataModel::getCourseCopy(const QUuid& courseId) const
 {
-	CoursePtr copy;
+	std::shared_ptr<Course> copy;
 	if (isValidCourse(courseId))
 	{
-		copy = Course::clone(mCourseMap.value(courseId));
+		copy = Course::clone(*mCourseMap.at(courseId));
 	}
 
 	return copy;
@@ -137,40 +212,40 @@ CoursePtr DataModel::getCourseCopy(const QUuid& courseId) const
 
 int DataModel::getLessonCount(const QUuid& courseId) const
 {
-	if(isValidCourse(courseId))
-		return mCourseMap.value(courseId)->size();
+	if (isValidCourse(courseId))
+		return mCourseMap.at(courseId)->size();
 	else
 		return 0;
 }
 
 QString DataModel::getLessonTitle(const QUuid& courseId, const QUuid& lessonId) const
 {
-	if(isValidLesson(courseId, lessonId))
-		return mCourseMap.value(courseId)->get(lessonId)->getTitle();
+	if (isValidLesson(courseId, lessonId))
+		return mCourseMap.at(courseId)->get(lessonId)->getTitle();
 	else
 		return QString();
 }
 
 QString DataModel::getLessonNewChars(const QUuid& courseId, const QUuid& lessonId) const
 {
-	if(isValidLesson(courseId, lessonId))
-		return mCourseMap.value(courseId)->get(lessonId)->getNewChars();
+	if (isValidLesson(courseId, lessonId))
+		return mCourseMap.at(courseId)->get(lessonId)->getNewChars();
 	else
 		return QString();
 }
 
 bool DataModel::isLessonBuiltin(const QUuid& courseId, const QUuid& lessonId) const
 {
-	if(isValidLesson(courseId, lessonId))
-		return mCourseMap.value(courseId)->get(lessonId)->isBuiltin();
+	if (isValidLesson(courseId, lessonId))
+		return mCourseMap.at(courseId)->get(lessonId)->isBuiltin();
 	else
 		return false;
 }
 
 QString DataModel::getLessonText(const QUuid& courseId, const QUuid& lessonId) const
 {
-	if(isValidLesson(courseId, lessonId))
-		return mCourseMap.value(courseId)->get(lessonId)->getText();
+	if (isValidLesson(courseId, lessonId))
+		return mCourseMap.at(courseId)->get(lessonId)->getText();
 	else
 		return QString();
 }
--- src/datamodel.hpp
+++ src/datamodel.hpp
@@ -8,33 +8,30 @@
 #ifndef DATAMODEL_HPP_
 #define DATAMODEL_HPP_
 
+#include <map>
 #include <QObject>
-#include <QMap>
 
 #include "db/db.hpp"
 
 namespace qtouch
 {
 
-class DataModel;
-typedef QSharedPointer<DataModel> DataModelPtr;
-
 class DataModel: public QObject
 {
 	Q_OBJECT
 
 public:
-	DataModel();
+	explicit DataModel(QObject* parent = 0);
 	virtual ~DataModel();
 
 	void init() throw (Exception);
 
 	// Course
 
-	int getCourseCount() const;
+	int getCourseCount() const { return mCourses.size(); }
 	inline bool isValidCourseIndex(int index) const
 	{
-		if (index >= 0 && index < mCourses.size())
+		if (index >= 0 && index < static_cast<int>(mCourses.size()))
 			return true;
 		else
 			return false;
@@ -42,50 +39,41 @@ public:
 
 	inline QUuid getCourseId(int index) const
 	{
-		if (!isValidCourseIndex(index))
-			return QUuid();
-
-		return mCourses.at(index)->getId();
+		if (!isValidCourseIndex(index))	return QUuid();
+		else return mCourses.at(index)->getId();
 	}
 
 	inline bool isValidCourse(const QUuid& courseId) const
 	{
-		if (mCourseMap.contains(courseId))
-			return true;
-		else
-			return false;
+		if (mCourseMap.count(courseId)) return true;
+		else return false;
 	}
 
 	QString getCourseTitle(const QUuid& courseId) const;
 	QString getCourseDescription(const QUuid& courseId) const;
 	bool isCourseBuiltin(const QUuid& courseId) const;
 
-	CoursePtr getCourseCopy(const QUuid& courseId) const;
+	std::shared_ptr<Course> getCourseCopy(const QUuid& courseId) const;
 
 	// Lesson
 
 	int getLessonCount(const QUuid& courseId) const;
 	inline bool isValidLessonIndex(const QUuid& courseId, int index) const
 	{
-		if (isValidCourse(courseId) && index >= 0 && index < mCourseMap.value(courseId)->size())
-			return true;
-		else
-			return false;
+		if (isValidCourse(courseId) && index >= 0 && index < mCourseMap.at(courseId)->size()) return true;
+		else return false;
 	}
+
 	inline QUuid getLessonId(const QUuid& courseId, int index) const
 	{
-		if (isValidLessonIndex(courseId, index))
-			return mCourseMap.value(courseId)->at(index)->getId();
-		else
-			return QUuid();
+		if (isValidLessonIndex(courseId, index)) return mCourseMap.at(courseId)->at(index)->getId();
+		else return QUuid();
 	}
 
 	inline bool isValidLesson(const QUuid& courseId, const QUuid& lessonId) const
 	{
-		if (isValidCourse(courseId) && mCourseMap.value(courseId)->contains(lessonId))
-			return true;
-		else
-			return false;
+		if (isValidCourse(courseId) && mCourseMap.at(courseId)->contains(lessonId))	return true;
+		else return false;
 	}
 
 	QString getLessonTitle(const QUuid& courseId, const QUuid& lessonId) const;
@@ -94,10 +82,10 @@ public:
 	QString getLessonText(const QUuid& courseId, const QUuid& lessonId) const;
 
 private:
-	QScopedPointer<Db> db;
+	std::unique_ptr<Db> db;
 
-	ConstCourseList mCourses;
-	ConstCourseMap mCourseMap;
+	std::vector<std::shared_ptr<Course>> mCourses;
+	std::map<QUuid, std::shared_ptr<Course>> mCourseMap;
 };
 
 } /* namespace qtouch */
